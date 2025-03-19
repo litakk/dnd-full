fa8e0a6d9178c3c105a366b9e59852a541aec4bf
Feruz <140828663+litakk@users.noreply.github.com>
2025-03-19 12:02:09 +0500

    upd

--- src/components/custom/Edit.tsx
+++ src/components/custom/Edit.tsx
@@ -1,13 +1,43 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface EditProps {
   closeModal: () => void;
+  task: { title: string; id: string } | null;
 }
 
-const Edit: React.FC<EditProps> = ({ closeModal }) => {
-  const onEdit = () => {};
+const Edit: React.FC<EditProps> = ({ closeModal, task }) => {
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState(task?.title || "");
+
+  const editBtn = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!task) return;
+
+    try {
+      const res = await fetch(`http://localhost:8080/todos/${task.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title }),
+      });
+
+      if (res.ok) {
+        closeModal();
+        navigate("/");
+      } else {
+        console.log("error reload");
+      }
+    } catch (error) {
+      console.error("Ошибка при отправке запроса:", error);
+    }
+  };
 
   return (
     <>
@@ -20,7 +50,7 @@ const Edit: React.FC<EditProps> = ({ closeModal }) => {
       {/* Модальное окно */}
       <div className="fixed inset-0 flex items-center justify-center z-20">
         <div className="w-[50%]">
-          <form onSubmit={onEdit} action="">
+          <form onSubmit={editBtn} action="">
             <Card>
               {/* Заголовок модального окна */}
               <CardHeader className="relative p-6 border-b">
@@ -41,11 +71,18 @@ const Edit: React.FC<EditProps> = ({ closeModal }) => {
               <CardContent className="space-y-4 p-6">
                 <div>
                   <p className="mb-2 text-gray-700">Описание задачи</p>
-                  <Input name="title" placeholder="Введите описание задачи" />
+                  <Input
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="Введите описание задачи"
+                  />
                 </div>
 
                 <div className="flex justify-end">
-                  <Button variant="outline">Добавить задачу</Button>
+                  <Button type="submit" variant="outline">
+                    Сохранить изменения
+                  </Button>
                 </div>
               </CardContent>
             </Card>
